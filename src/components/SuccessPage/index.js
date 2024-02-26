c6f32b6caed181f351289a9b46b80f1dfdeeb7d0
yenukotipriyanka <yenukotipriyanka@gmail.com>
2024-02-26 18:38:31 +0000

    solution

--- src/components/SuccessPage/index.js
+++ src/components/SuccessPage/index.js
@@ -1,8 +1,3 @@
-const SuccessPage = ({name}) => (
-  <div>
-    <h2>Welcome, {name}!</h2>
-    <p>You have successfully signed up.</p>
-  </div>
-)
+const SuccessPage = () => <h2>Successfully Signed Up!</h2>
 
 export default SuccessPage
